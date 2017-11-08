5536220d4e99d5b405b304fab358daa50fd87e80
curogihu <curoasg@gmail.com>
2017-11-09 08:23:41 +0900

    add api url plan

--- laravel_projects/LoLCounterBuild/routes/web.php
+++ laravel_projects/LoLCounterBuild/routes/web.php
@@ -28,3 +28,11 @@ Route::get('/champion/statistics', function () {
 Route::get('/champion/statistics/{champion_name}', 'StatisticsController@create');
 
 Route::get('/champion/{champion_name}/statistics/{role}/{enemy_champion_name}', 'StatisticsController@index')->name('statistics');
+
+
+/*
+/api/v1/self/{champion_name}?token=??? -> get existed lanes info
+/api/v1/self/{champion_name}/{lane}?token=??? -> get existed enemies info
+/api/v1/self/{champion_name}/{lane}/enemy/{enemy-champion}?token=??? -> get counter win lose build incluing all ranks
+/api/v1/self/{champion_name}/{lane}/enemy/{enemy-champion}?token=???&rank=bronze,silver,challenger -> get counter win lose build including particular ranks
+*/
